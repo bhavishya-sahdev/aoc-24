38652cfae7b9427ff1f0d02d0ae3cd4652a32fc0
Bhavishya <bhavishya1102@gmail.com>
2024-12-12 17:21:25 +0530

    add day 3 part 2

--- src/day3/main.rs
+++ src/day3/main.rs
@@ -8,12 +8,23 @@ pub fn main() {
 }
 
 fn extract_product(content: String) -> u32 {
-    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)|don't\(\)|do\(\)").unwrap();
     let mut res = 0;
+    let mut enabled = true;
     for cap in regex.captures_iter(&content) {
-        let num1: u32 = cap[1].parse().unwrap();
-        let num2: u32 = cap[2].parse().unwrap();
-        res += num1 * num2;
+        if let Some(mul_match) = cap.get(1) {
+            if !enabled {
+                continue;
+            }
+
+            let num1: u32 = mul_match.as_str().parse().unwrap();
+            let num2: u32 = cap[2].parse().unwrap();
+            res += num1 * num2;
+        } else if cap.get(0).unwrap().as_str() == "don't()" {
+            enabled = false;
+        } else if cap.get(0).unwrap().as_str() == "do()" {
+            enabled = true;
+        }
     }
     res
 }
