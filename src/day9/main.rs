165ef3dd5d40a467deaaf18135474597d48a6753
Bhavishya <bhavishya1102@gmail.com>
2024-12-17 14:39:54 +0530

    day 9 part 2

--- src/day9/main.rs
+++ src/day9/main.rs
@@ -15,44 +15,67 @@ pub fn main() {
     println!("{:?}", disk_fragmenter(&disk));
 }
 
+/**
+ * referred https://github.com/nitekat1124/advent-of-code-2024/blob/main/solutions/day09.py
+ */
 pub fn disk_fragmenter(content: &DiskType) -> i64 {
-    let mut content = content.clone();
-    let mut disk: Vec<i32> = vec![];
+    let mut blocks: Vec<(usize, Vec<i32>, i32)> = Vec::new();
+    let mut file_id = 0;
+    let mut space_indices: Vec<usize> = Vec::new();
 
-    let mut c: i32 = 0;
-    let mut i = 0;
-    let mut y = if content.len() % 2 == 0 {
-        content.len() - 2
-    } else {
-        content.len() - 1
-    };
-
-    while i <= y {
+    for (i, &count) in content.iter().enumerate() {
         if i % 2 == 0 {
-            while content[i] > 0 {
-                disk.push(c);
-                content[i] -= 1;
+            if count > 0 {
+                blocks.push((1, vec![file_id; count as usize], 0));
+                file_id += 1;
             }
-            c += 1;
-        } else {
-            while content[i] > 0 && y > i {
-                if content[y] > 0 {
-                    disk.push(y as i32 / 2);
-                    content[y] -= 1;
-                } else {
-                    y -= 2;
-                    continue;
-                }
-                content[i] -= 1;
-            }
+        } else if count > 0 {
+            space_indices.push(blocks.len());
+            blocks.push((0, Vec::new(), count));
         }
-        i += 1;
     }
 
-    let mut sum: i64 = 0;
-    (0..disk.len()).for_each(|i| {
-        sum += (i as i64) * (disk[i] as i64);
-    });
+    let mut curr_block_idx = blocks.len() - 1;
+    while curr_block_idx > 0 {
+        if blocks[curr_block_idx].0 == 0 {
+            curr_block_idx -= 1;
+            continue;
+        }
 
-    sum
+        let curr_block_len = blocks[curr_block_idx].1.len();
+
+        for &space_idx in space_indices.iter() {
+            if space_idx >= curr_block_idx {
+                break;
+            }
+
+            if blocks[space_idx].2 >= curr_block_len as i32 {
+                let (left, right) = blocks.split_at_mut(curr_block_idx);
+                left[space_idx].1.append(&mut right[0].1);
+                left[space_idx].2 -= curr_block_len as i32;
+
+                right[0].0 = 0;
+                blocks[curr_block_idx].2 = curr_block_len as i32;
+
+                break;
+            }
+        }
+
+        curr_block_idx -= 1;
+    }
+
+    let mut checksum = 0i64;
+    let mut pos = 0i64;
+
+    for block in blocks {
+        for file in block.1 {
+            checksum += pos * file as i64;
+            pos += 1;
+        }
+        if block.0 == 0 {
+            pos += block.2 as i64;
+        }
+    }
+
+    checksum
 }
